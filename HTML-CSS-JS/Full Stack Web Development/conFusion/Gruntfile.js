cdd83b47bc13bb71a1273d184a8142c7029efae0
Hankang Yang <hancock.yang@gmail.com>
2017-01-11 15:27:36 -0500

    gruntfile

--- HTML-CSS-JS/Full Stack Web Development/conFusion/Gruntfile.js
+++ HTML-CSS-JS/Full Stack Web Development/conFusion/Gruntfile.js
@@ -11,22 +11,59 @@ module.exports = function (grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		// Make sure code styles are up to par and there are no obvious mistakes
 		jshint: {
-	    options: {
-	      jshintrc: '.jshintrc',
-	      reporter: require('jshint-stylish')
-	    },
+		    options: {
+		      jshintrc: '.jshintrc',
+		      reporter: require('jshint-stylish')
+		    },
 	    
-	    all: {
-	      src: [
-	        'Gruntfile.js',
-	        'app/scripts/{,*/}*.js'
-	      ]
-	    }
-	  }
+		    all: {
+		      src: [
+		        'Gruntfile.js',
+		        'app/scripts/{,*/}*.js'
+		      ]
+		    }
+	  	},
+
+	  	copy: {
+		  dist: {
+		    cwd: 'app',
+		    src: [ '**','!styles/**/*.css','!scripts/**/*.js' ],
+		    dest: 'dist',
+		    expand: true
+		  },
+		  
+		  fonts: {
+		    files: [
+		      {
+		        //for bootstrap fonts
+		        expand: true,
+		        dot: true,
+		        cwd: 'bower_components/bootstrap/dist',
+		        src: ['fonts/*.*'],
+		        dest: 'dist'
+		      }, {
+		        //for font-awesome
+		        expand: true,
+		        dot: true,
+		        cwd: 'bower_components/font-awesome',
+		        src: ['fonts/*.*'],
+		        dest: 'dist'
+		      }
+		    ]
+		  }
+		},
+
+		clean: {
+		  build: {
+		    src: [ 'dist/']
+		  }
+		}
 	});
-	
+
 	grunt.registerTask('build', [
-	  'jshint'
+		'clean',
+	  	'jshint',
+	  	'copy'
 	]);
 
 	grunt.registerTask('default',['build']);
