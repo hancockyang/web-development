12a625f9746a335ec43dfe5334dcbd28a716335b
Hankang Yang <hancock.yang@gmail.com>
2017-01-16 22:50:52 -0500

    Task2

--- HTML-CSS-JS/Full Stack Web Development/conFusion/app/scripts/app.js
+++ HTML-CSS-JS/Full Stack Web Development/conFusion/app/scripts/app.js
@@ -1,9 +1,10 @@
 'use strict';
-angular.module('confusionApp', []).controller('MenuController',['$scope', function($scope) {
-          $scope.tab = 1;
-          $scope.filtText = '';
-          
-          $scope.dishes = [
+angular.module('confusionApp', [])
+    .controller('MenuController',['$scope', function($scope) {
+        $scope.tab = 1;
+        $scope.filtText = '';
+        
+        $scope.dishes = [
             {
               name:'Uthapizza',
               image: 'images/uthapizza.png',
@@ -13,7 +14,7 @@ angular.module('confusionApp', []).controller('MenuController',['$scope', functi
               description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
               comment: ''
             },
-            
+          
             {
               name:'Zucchipakoda',
               image: 'images/zucchipakoda.png',
@@ -23,7 +24,7 @@ angular.module('confusionApp', []).controller('MenuController',['$scope', functi
               description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
               comment: ''
             },
-            
+          
             {
               name:'Vadonut',
               image: 'images/vadonut.png',
@@ -33,7 +34,7 @@ angular.module('confusionApp', []).controller('MenuController',['$scope', functi
               description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
               comment: ''
             },
-            
+          
             {
               name:'ElaiCheese Cake',
               image: 'images/elaicheesecake.png',
@@ -43,11 +44,11 @@ angular.module('confusionApp', []).controller('MenuController',['$scope', functi
               description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
               comment: ''
             }
-          ]; 
-          
-          
+        ]; 
+        
+        
 
-          $scope.select = function(setTab) {
+        $scope.select = function(setTab) {
             $scope.tab = setTab;
             
             if (setTab === 2) {
@@ -62,34 +63,33 @@ angular.module('confusionApp', []).controller('MenuController',['$scope', functi
             else {
               $scope.filtText = "";
             }
-          };
+        };
 
-          $scope.showDetails = false;
-          $scope.toggleDetails = function() {
+        $scope.showDetails = false;
+        $scope.toggleDetails = function() {
             $scope.showDetails = !$scope.showDetails;
-          };
-          
-          $scope.isSelected = function (checkTab) {
+        };
+        
+        $scope.isSelected = function (checkTab) {
             return ($scope.tab === checkTab);
-          };
-        }])
+        };
+    }])
 
-        .controller('ContactController', ['$scope', function($scope) {
+    .controller('ContactController', ['$scope', function($scope) {
 
-            $scope.feedback = {mychannel:"", firstName:"", lastName:"",
-                               agree:false, email:"" };
-            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-            $scope.channels = channels;
-            $scope.invalidChannelSelection = false;
-        }])
+        $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+        var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+        $scope.channels = channels;
+        $scope.invalidChannelSelection = false;
+    }])
 
-        .controller('FeedbackController', ['$scope', function($scope) {
+    .controller('FeedbackController', ['$scope', function($scope) {
 
-                $scope.sendFeedback = function() {
+            $scope.sendFeedback = function() {
                 console.log($scope.feedback);
                 if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
-                  $scope.invalidChannelSelection = true;
-                  console.log('incorrect');
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
                 }
                 else {
                     $scope.invalidChannelSelection = false;
@@ -99,11 +99,77 @@ angular.module('confusionApp', []).controller('MenuController',['$scope', functi
 
                     $scope.feedbackForm.$setPristine();
                     console.log($scope.feedback);
-                }
+                };
             };
 
 
-        }]);
+    }])
 
+
+    .controller('DishDetailController', ['$scope', function($scope) {
+        $scope.dish={
+            name:'Uthapizza',
+            image: 'images/uthapizza.png',
+            category: 'mains', 
+            label:'Hot',
+            price:'4.99',
+            description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comments: [
+                 {
+                     rating:5,
+                     comment:"Imagine all the eatables, living in conFusion!",
+                     author:"John Lemon",
+                     date:"2012-10-16T17:57:28.556094Z"
+                 },
+                 {
+                     rating:4,
+                     comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                     author:"Paul McVites",
+                     date:"2014-09-05T17:57:28.556094Z"
+                 },
+                 {
+                     rating:3,
+                     comment:"Eat it, just eat it!",
+                     author:"Michael Jaikishan",
+                     date:"2015-02-13T17:57:28.556094Z"
+                 },
+                 {
+                     rating:4,
+                     comment:"Ultimate, Reaching for the stars!",
+                     author:"Ringo Starry",
+                     date:"2013-12-02T17:57:28.556094Z"
+                 },
+                 {
+                     rating:2,
+                     comment:"It's your birthday, we're gonna party!",
+                     author:"25 Cent",
+                     date:"2011-12-02T17:57:28.556094Z"
+                 }
+                 
+            ]
+        };
+            
+            
+        $scope.sortOption = '';
+
+    }])
+
+    .controller('DishCommentController', ['$scope', function($scope) {
+            
+        $scope.Comment = {Name:'', Stars:"5" };
+        //console.log(Comment.Name.$invalid);
+        $scope.submitComment = function () {
+            
+            //Step 2: This is how you record the date
+            //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
+            
+            // Step 3: Push your comment into the dish's comment array
+            $scope.dish.comments.push("Your JavaScript Object holding the comment");
+            
+            //Step 4: reset your form to pristine
+            
+            //Step 5: reset your JavaScript object that holds your comment
+        }
+    }]); 
 
 ;
