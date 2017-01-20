006eb4f8629a737705fff53feb7b3386baa9fcea
Hankang Yang <hancock.yang@gmail.com>
2017-01-19 20:24:55 -0500

    new

--- HTML-CSS-JS/Full Stack Web Development/conFusion/app/scripts/controllers.js
+++ HTML-CSS-JS/Full Stack Web Development/conFusion/app/scripts/controllers.js
@@ -66,11 +66,9 @@ angular.module('confusionApp')
     }])
 
 
-    .controller('DishDetailController', ['$scope', 'menuFactory', function($scope,menuFactory) {
-        $scope.dish= menuFactory.getDish(3);
-            
-            
-        $scope.sortOption = '';
+    .controller('DishDetailController', ['$scope','$routeParams', 'menuFactory', function($scope,$routeParams,menuFactory) {
+        var dish= menuFactory.getDish(parseInt($routeParams.id,10));                       
+        $scope.dish = dish;
 
     }])
 
